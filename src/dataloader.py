092d1f73a1b49539f0f482790dbbfc0bbf12a302
nathaniel <nathaniel.simard.42@gmail.com>
2020-03-21 15:28:57 -0400

    Finish dataset ishh

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,6 +1,28 @@
+from typing import List
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from typing import List
+
+
+class Dataloader:
+    def __init__(self, file_name_input: str, file_name_output: str, vocab_size: int):
+        self.file_name_input = file_name_input
+        self.file_name_output = file_name_output
+        self.vocab_size = vocab_size
+
+    def create_dataset(self) -> tf.data.Dataset:
+        """Create the Tensorflow dataset."""
+        corpus_input = read_file(self.file_name_input)
+        corpus_output = read_file(self.file_name_output)
+
+        encoder_input = create_encoder(corpus_input, self.vocab_size)
+        encoder_ouput = create_encoder(corpus_output, self.vocab_size)
+
+        def gen():
+            for i, o in zip(corpus_input, corpus_output):
+                yield (encoder_input.encode(i), encoder_ouput.encode(o))
+
+        return tf.data.Dataset.from_generator(gen, (tf.int64, tf.int64))
 
 
 def read_file(file_name: str) -> List[str]:
@@ -20,9 +42,3 @@ def create_encoder(
     return tfds.features.text.SubwordTextEncoder.build_from_corpus(
         (sentence for sentence in sentences), target_vocab_size=max_vocab_size
     )
-
-
-def create_dataset(
-    encoder: tfds.features.text.TextEncoder, data: List[str]
-) -> tf.data.Dataset:
-    """Create the Tensorflow dataset."""
--- tests/dataloader_test.py
+++ tests/dataloader_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 from src import dataloader
 
 ANY_TEXT_FILE = "data/train.lang1"
@@ -13,10 +14,21 @@ class DataloaderTest(unittest.TestCase):
 
     def test_create_encoder_from_sentences(self):
         corpus = ["a against", "battle", "pandemy"]
-
         sample = "a battle"
 
         encoder = dataloader.create_encoder(corpus, 258)
+
         ids = encoder.encode(sample)
         out = encoder.decode(ids)
         self.assertEqual(out, sample)
+
+    def test_create_dataset(self):
+        dl = dataloader.Dataloader("tests/sample1.txt", "tests/sample2.txt", 300)
+        dataset = dl.create_dataset()
+
+        samples_num = 0
+        for sample in dataset.padded_batch(1, padded_shapes=([None], [None])):
+            samples_num += 1
+            self.assertTrue(sample is not None)
+
+        self.assertEqual(25, samples_num)
