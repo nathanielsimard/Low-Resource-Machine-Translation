e24863793263cfc34038b78e9da072cc41de22ee
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-01 10:54:07 -0400

    Fix missing return 🤷

--- src/text_encoder.py
+++ src/text_encoder.py
@@ -203,3 +203,5 @@ def _create_text_encoder(text: List[str], vocab_size: int, clazz, cache_file=Non
     except FileNotFoundError:
         encoder = clazz(vocab_size, text)
         encoder.save_to_file(cache_file)
+
+        return encoder
