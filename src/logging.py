f97c9bc210d13d9d64a77d194c8a66d589e90d0b
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-01 13:43:39 -0400

    Use Toraring file handler

--- src/logging.py
+++ src/logging.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
 LEVEL = logging.INFO
 
@@ -11,17 +12,20 @@ if "DEBUG" in os.environ:
 
 directory = os.path.join("logging", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 os.makedirs(directory, exist_ok=True)
+
 file_name = os.path.join(directory, "experiment.log")
-file = open(file_name, "w")
+
+formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+# Create a handler that rotate files each 512mb
+handler = RotatingFileHandler(
+    file_name, mode="a", maxBytes=536_870_912, backupCount=4, encoding=None
+)
+handler.setFormatter(formatter)
 
 
 def create_logger(name: str) -> logging.Logger:
     """Create a logger with default configuration and formatter."""
     logger = logging.getLogger(name)
-    formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-    handler = logging.StreamHandler(stream=file)
-    handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(LEVEL)
-
     return logger
