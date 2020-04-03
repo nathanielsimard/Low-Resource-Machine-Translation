7147739711b9eec21a21d6e8c40d96ce10e3a885
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-03 14:59:55 -0400

    Refactor run experiment

--- run_experiment.py
+++ run_experiment.py
@@ -4,12 +4,11 @@ import random
 import tensorflow as tf
 
 from src import dataloader, logging
-from src.model import gru_attention, lstm, transformer, lstm_luong_attention
+from src.model import gru_attention, lstm, lstm_luong_attention, transformer
 from src.text_encoder import TextEncoderType
 from src.training import base
 from src.training.back_translation import BackTranslationTraining
 from src.training.base import BasicMachineTranslationTraining
-
 
 logger = logging.create_logger(__name__)
 # Embedding for models have to be vocab size + 1 because of the
@@ -53,109 +52,21 @@ MODELS = {
 }
 
 
-def parse_args():
-    """Parse the user's arguments.
-
-    The default arguments are to be used in order to reproduce
-    the original experiments.
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--epochs", help="Number of epoch to basic_training", default=25, type=int
-    )
-    parser.add_argument(
-        "--test",
-        help="Test a trained model on the test set. The value must be the model's checkpoint",
-        default=None,
-        type=str,
-    )
-    parser.add_argument(
-        "--basic_training", help="Train a model.", action="store_true",
-    )
-    parser.add_argument(
-        "--back_translation_training",
-        help="Train a model with back translation.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--seed", help="Seed for the experiment", default=1234, type=int
-    )
-    parser.add_argument(
-        "--random_seed",
-        help="Will overide the default seed and use a random one",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--checkpoint",
-        help="The checkpoint to load before training.",
-        default=None,
-        type=int,
-    )
-    parser.add_argument("--lr", help="Learning rate", default=0.001, type=float)
-    parser.add_argument(
-        "--text_encoder", help="Text Encoder type", default="word", type=str
-    )
-    parser.add_argument(
-        "--model",
-        help=f"Name of the model to run, available models are:\n{list(MODELS.keys())}",
-        type=str,
-        required=True,
-    )
-    parser.add_argument("--batch_size", help="Batch size", default=16, type=int)
-    parser.add_argument(
-        "--max_seq_lenght", help="Max sequence lenght", default=None, type=int
-    )
-    parser.add_argument(
-        "--vocab_size", help="Size of the vocabulary", default=80000, type=int
-    )
-    return parser.parse_args()
-
-
-def main():
-    args = parse_args()
-
-    if args.basic_training and args.back_translation_training:
-        raise ValueError(
-            "Both basic training and back translation training were chosen, only one can be use at the same time."
+def find_model(args, input_vocab_size, target_vocab_size):
+    try:
+        return MODELS[args.model](args, input_vocab_size, target_vocab_size)
+    except KeyError as e:
+        logger.error(
+            f"Model {args.model} is not supported, available models are {list(MODELS.keys())}."
         )
-
-    if not args.random_seed:
-        random.seed(args.seed)
-        tf.random.set_seed(args.seed)
-
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction="none"
-    )
-
-    def loss_function(real, pred):
-        mask = tf.math.logical_not(tf.math.equal(real, 0))
-        loss_ = loss_object(real, pred)
-
-        mask = tf.cast(mask, dtype=loss_.dtype)
-        loss_ *= mask
-
-        return tf.reduce_mean(loss_)
-
-    if args.basic_training:
-        basic_training(args, loss_function)
-
-    if args.back_translation_training:
-        back_translation_training(args, loss_function)
-
-    if args.test is not None:
-        test(args, loss_function)
+        raise ValueError(e)
 
 
-def basic_training(args, loss_fn):
-    """Train the model."""
+def punctuation_training(args, loss_fn):
+    """Train the model for the punctuation task."""
     text_encoder_type = TextEncoderType(args.text_encoder)
 
     optim = tf.keras.optimizers.Adam(learning_rate=args.lr)
-    # "data/splitted_data/sorted_train_token.en"
-    # "data/splitted_data/sorted_nopunctuation_lowercase_train_token.fr"
-
-    # "data/splitted_data/sorted_val_token.en"
-    # "data/splitted_data/sorted_nopunctuation_lowercase_val_token.fr"
     train_dl = dataloader.AlignedDataloader(
         file_name_input="data/splitted_english_data/sorted_clean_train.en",
         file_name_target="data/splitted_english_data/sorted_target_train.en",
@@ -172,10 +83,46 @@ def basic_training(args, loss_fn):
         encoder_target=train_dl.encoder_target,
         max_seq_lenght=args.max_seq_lenght,
     )
-    model = MODELS[args.model](
+    model = find_model(
         args, train_dl.encoder_input.vocab_size, train_dl.encoder_target.vocab_size
     )
-    training = BasicMachineTranslationTraining(model, train_dl, valid_dl)
+    training = BasicMachineTranslationTraining(model, train_dl, valid_dl, [])
+    training.run(
+        loss_fn,
+        optim,
+        batch_size=args.batch_size,
+        num_epoch=args.epochs,
+        checkpoint=args.checkpoint,
+    )
+
+
+def default_training(args, loss_fn):
+    """Train the model."""
+    text_encoder_type = TextEncoderType(args.text_encoder)
+
+    optim = tf.keras.optimizers.Adam(learning_rate=args.lr)
+    train_dl = dataloader.AlignedDataloader(
+        file_name_input="data/splitted_data/sorted_train_token.en",
+        file_name_target="data/splitted_data/sorted_nopunctuation_lowercase_train_token.fr",
+        vocab_size=args.vocab_size,
+        text_encoder_type=text_encoder_type,
+        max_seq_lenght=args.max_seq_lenght,
+    )
+    valid_dl = dataloader.AlignedDataloader(
+        file_name_input="data/splitted_data/sorted_val_token.en",
+        file_name_target="data/splitted_data/sorted_nopunctuation_lowercase_val_token.fr",
+        vocab_size=args.vocab_size,
+        text_encoder_type=text_encoder_type,
+        encoder_input=train_dl.encoder_input,
+        encoder_target=train_dl.encoder_target,
+        max_seq_lenght=args.max_seq_lenght,
+    )
+    model = find_model(
+        args, train_dl.encoder_input.vocab_size, train_dl.encoder_target.vocab_size
+    )
+    training = BasicMachineTranslationTraining(
+        model, train_dl, valid_dl, [base.Metrics.BLEU]
+    )
     training.run(
         loss_fn,
         optim,
@@ -252,13 +199,13 @@ def back_translation_training(args, loss_fn):
         max_seq_lenght=args.max_seq_lenght,
     )
 
-    model = MODELS[args.model](
+    model = find_model(
         args,
         aligned_train_dl.encoder_input.vocab_size,
         aligned_train_dl.encoder_target.vocab_size,
     )
 
-    model_reverse = MODELS[args.model](
+    model_reverse = find_model(
         args,
         aligned_train_dl_reverse.encoder_input.vocab_size,
         aligned_train_dl_reverse.encoder_target.vocab_size,
@@ -304,11 +251,103 @@ def test(args, loss_fn):
         text_encoder_type=text_encoder_type,
         max_seq_lenght=args.max_seq_lenght,
     )
-    model = MODELS[args.model](
+    model = find_model(
         args, train_dl.encoder_input.vocab_size, train_dl.encoder_target.vocab_size
     )
     base.test(model, loss_fn, test_dl, args.batch_size, args.test)
 
 
+TASK = {
+    "default-training": default_training,
+    "punctuation-training": punctuation_training,
+    "back-translation-training": back_translation_training,
+    "test": test,
+}
+
+
+def parse_args():
+    """Parse the user's arguments.
+
+    The default arguments are to be used in order to reproduce
+    the original experiments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--epochs", help="Number of epoch to default_training", default=25, type=int
+    )
+    parser.add_argument(
+        "--task",
+        help=f"Type of training to execute. possible ones are:\n{list(TASK.keys())}",
+        default="default-training",
+        type=str,
+    )
+    parser.add_argument(
+        "--seed", help="Seed for the experiment", default=1234, type=int
+    )
+    parser.add_argument(
+        "--random_seed",
+        help="Will overide the default seed and use a random one",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--checkpoint",
+        help="The checkpoint to load before training.",
+        default=None,
+        type=int,
+    )
+    parser.add_argument("--lr", help="Learning rate", default=0.001, type=float)
+    parser.add_argument(
+        "--text_encoder", help="Text Encoder type", default="word", type=str
+    )
+    parser.add_argument(
+        "--model",
+        help=f"Name of the model to run, available models are:\n{list(MODELS.keys())}",
+        type=str,
+        required=True,
+    )
+    parser.add_argument("--batch_size", help="Batch size", default=16, type=int)
+    parser.add_argument(
+        "--max_seq_lenght", help="Max sequence lenght", default=None, type=int
+    )
+    parser.add_argument(
+        "--vocab_size", help="Size of the vocabulary", default=30000, type=int
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    if not args.random_seed:
+        random.seed(args.seed)
+        tf.random.set_seed(args.seed)
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction="none"
+    )
+
+    def loss_function(real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = loss_object(real, pred)
+
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
+
+        return tf.reduce_mean(loss_)
+
+    try:
+        logger.info(f"Executing task {args.task}.")
+        training = TASK[args.task]
+        training(args, loss_function)
+    except KeyError:
+        logger.error(
+            f"Task {args.task} is not supported, available tasks are {TASK.keys()}."
+        )
+
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except ValueError:
+        # Logging is already done
+        pass
