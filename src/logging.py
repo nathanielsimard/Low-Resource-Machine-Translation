277e4f0761882dd62081159e7337826321b286b9
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2020-04-04 11:18:11 -0400

    Refactor punctuation (#39)
    * Refactor run experiment
    
    * Brain dump
    
    * log arguments
    
    * order import
    
    * Cleanup
    
    * Fix CI
    
    Co-authored-by: jessica <jessgauvin@hotmail.com>

--- src/logging.py
+++ src/logging.py
@@ -3,29 +3,53 @@ import os
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
-LEVEL = logging.INFO
-
-if "DEBUG" in os.environ:
-    if os.environ["DEBUG"] == "1":
-        LEVEL = logging.DEBUG
+handler = None
+level = None
 
 
-directory = os.path.join("logging", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-os.makedirs(directory, exist_ok=True)
+def initialize(experiment_name="experiment", debug=False):
+    """Initialize the logging module.
 
-file_name = os.path.join(directory, "experiment.log")
+    It can be called before any logger is created to change the default arguments.
+    """
+    directory = os.path.join(
+        "logging/" + experiment_name, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
+    os.makedirs(directory, exist_ok=True)
 
-formatter = logging.Formatter("[%(asctime)s - %(levelname)s - %(name)s] %(message)s")
-# Create a handler that rotate files each 512mb
-handler = RotatingFileHandler(
-    file_name, mode="a", maxBytes=536_870_912, backupCount=4, encoding=None
-)
-handler.setFormatter(formatter)
+    file_name = os.path.join(directory, "experiment.log")
+
+    _initialize_level(debug)
+    _initialize_handler(file_name)
+
+
+def _initialize_handler(file_name):
+    global handler
+    formatter = logging.Formatter(
+        "[%(asctime)s - %(levelname)s - %(name)s] %(message)s"
+    )
+    # Create a handler that rotate files each 512mb
+    handler = RotatingFileHandler(
+        file_name, mode="a", maxBytes=536_870_912, backupCount=4, encoding=None
+    )
+    handler.setFormatter(formatter)
+
+
+def _initialize_level(debug):
+    global level
+    if debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
 
 
 def create_logger(name: str) -> logging.Logger:
     """Create a logger with default configuration and formatter."""
+    initialized = level is not None and handler is not None
+    if not initialized:
+        initialize()
+
     logger = logging.getLogger(name)
-    logger.addHandler(handler)
-    logger.setLevel(LEVEL)
+    logger.addHandler(handler)  # type: ignore
+    logger.setLevel(level)  # type: ignore
     return logger
