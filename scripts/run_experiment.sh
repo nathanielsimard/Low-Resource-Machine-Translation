dd042331f85ce9cf2aa552ed892c4504d81a7d12
Tryong <olivier10_10@hotmail.com>
2020-04-07 18:46:45 -0400

    script for helios

---
+++ scripts/run_experiment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:k80:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16000M
+
+module load python/3.7
+source /project/cq-training-1/project2/teams/team10/env/bin/activate
+
+python run_experiment.py $@
