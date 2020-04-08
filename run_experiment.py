7b533946ffb6c128623b52c62b59ca4a0138c0dd
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-08 16:59:34 +0000

    Fix run experiment

--- run_experiment.py
+++ run_experiment.py
@@ -60,14 +60,14 @@ def default_training(args, loss_fn):
     optim = tf.keras.optimizers.Adam(learning_rate=args.lr)
     train_dl = dataloader.AlignedDataloader(
         file_name_input=args.src_train,
-        file_name_target=args.tgt_train,
+        file_name_target=args.target_train,
         vocab_size=args.vocab_size,
         text_encoder_type=text_encoder_type,
         max_seq_length=args.max_seq_length,
     )
     valid_dl = dataloader.AlignedDataloader(
         file_name_input=args.src_valid,
-        file_name_target=args.tgt_valid,
+        file_name_target=args.target_valid,
         vocab_size=args.vocab_size,
         text_encoder_type=text_encoder_type,
         encoder_input=train_dl.encoder_input,
