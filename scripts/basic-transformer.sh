266a5769c691a0827a282bb271ab4be7d4c25ec8
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-09 15:59:04 -0400

    Add result

--- scripts/basic-transformer.sh
+++ scripts/basic-transformer.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # hyperparameters = {
-#        "num_layers": 6,
+#        "num_layers": 4,
 #        "num_heads": 4,
-#        "dff": 512,
-#        "d_model": 512,
+#        "dff": 256,
+#        "d_model": 256,
 #        "input_vocab_size": input_vocab_size + 1,
 #        "target_vocab_size": target_vocab_size + 1,
 #        "pe_input": input_vocab_size + 1,
 #        "pe_target": target_vocab_size + 1,
 #        "rate": 0.1,
 #    }
+
+# Bleu Score after 25 epochs:
+#   - Train 30.117
+#   - Valid 6.674
+#   - Test 6.816
 
 python run_experiment.py \
 	--model transformer \
