226d027be332e5278786ff5e5f701ed4b3b548ac
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-09 17:20:11 -0400

    Add script for transformer with subword

---
+++ scripts/basic-transformer-subword.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# hyperparameters = {
+#        "num_layers": 4,
+#        "num_heads": 4,
+#        "dff": 256,
+#        "d_model": 256,
+#        "input_vocab_size": input_vocab_size + 1,
+#        "target_vocab_size": target_vocab_size + 1,
+#        "pe_input": input_vocab_size + 1,
+#        "pe_target": target_vocab_size + 1,
+#        "rate": 0.1,
+#    }
+
+python run_experiment.py \
+	--model transformer \
+	--text_encoder subword \
+	--vocab_size 8192 \
+	--epochs 100 \
+	--lr None \
+	--batch_size 64 \
+	--max_seq_length 500 \
+	--src_train data/splitted_data/train/train_token10000.en \
+	--target_train data/splitted_data/train/train_token10000.fr \
+	--src_valid data/splitted_data/valid/val_token10000.en \
+	--target_valid data/splitted_data/valid/val_token10000.fr \
+	--no_cache
