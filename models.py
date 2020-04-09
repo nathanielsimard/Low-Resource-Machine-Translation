b598f005d3609c21ca98e0ccf5228e5a0660d0fc
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-09 10:43:38 -0400

    Add basic transformer experiment

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ import hashlib
 from collections import OrderedDict
 
 from src import logging
-from src.model import gru_attention, lstm, lstm_luong_attention, masked_lm, transformer
+from src.model import (gru_attention, lstm, lstm_luong_attention, masked_lm,
+                       transformer)
 
 logger = logging.create_logger(__name__)
 
@@ -17,9 +18,9 @@ def create_lstm(args, input_vocab_size, target_vocab_size):
 
 def create_transformer(args, input_vocab_size, target_vocab_size):
     hyperparameters = {
-        "num_layers": 2,
-        "num_heads": 2,
-        "dff": 256,
+        "num_layers": 4,
+        "num_heads": 8,
+        "dff": 512,
         "d_model": 256,
         "input_vocab_size": input_vocab_size + 1,
         "target_vocab_size": target_vocab_size + 1,
