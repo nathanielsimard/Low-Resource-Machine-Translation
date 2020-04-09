c02a6195d9b60e6a22442db41430a025fbc042fe
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-09 17:01:09 -0400

    Fix doc in sh

--- scripts/basic-transformer.sh
+++ scripts/basic-transformer.sh
@@ -21,8 +21,8 @@ python run_experiment.py \
 	--model transformer \
 	--text_encoder word-no-filter \
 	--vocab_size 30000 \
-	--epochs 100 \
-	--lr 0.0005 \
+	--epochs 25 \
+	--lr None \
 	--batch_size 64 \
 	--max_seq_length 500 \
 	--src_train data/splitted_data/train/train_token10000.en \
