637bc44a60ae2306d99df25d83daee9f978a70bc
Raphael Jean <raphael.jean@rocketmail.com>
2020-04-10 12:06:14 -0400

    Rebuild vocabulary at each experiment

--- custom_transformer_training.sh
+++ custom_transformer_training.sh
@@ -1,4 +1,4 @@
 VOCAB_SIZE=16000
-#onmt-build-vocab --size $VOCAB_SIZE  --save_vocab data/splitted_data/train/vocab10000.en data/splitted_data/train/train_token10000.en
-#onmt-build-vocab --size $VOCAB_SIZE  --save_vocab data/splitted_data/train/vocab10000.fr data/splitted_data/train/train_token10000.fr
+onmt-build-vocab --size $VOCAB_SIZE  --save_vocab data/splitted_data/train/vocab10000.en data/splitted_data/train/train_token10000.en
+onmt-build-vocab --size $VOCAB_SIZE  --save_vocab data/splitted_data/train/vocab10000.fr data/splitted_data/train/train_token10000.fr
 python custom_transformer_training.py --src data/splitted_data/train/train_token10000.en --tgt data/splitted_data/train/train_token10000.fr --src_vocab data/splitted_data/train/vocab10000.en --tgt_vocab data/splitted_data/train/vocab10000.fr train
