b380ffc04f9bf70f116e5b4b71c357f8959800d8
Nathaniel Simard <nathaniel.simard.42@gmail.com>
2020-04-11 11:31:36 -0400

    Basic transformer subword (#67)
    * Add script for transformer with subword
    
    * Add experiment results

---
+++ scripts/basic-transformer-subword.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# hyperparameters = {
+#        "num_layers": 4,
+#        "num_heads": 4,
+#        "dff": 256,
+#        "d_model": 256,
+#        "input_vocab_size": input_vocab_size + 1,
+#        "target_vocab_size": target_vocab_size + 1,
+#        "pe_input": input_vocab_size + 1,
+#        "pe_target": target_vocab_size + 1,
+#        "rate": 0.1,
+#    }
+
+# Bleu Score after 38 epochs:
+#   - Train 32.5439
+#   - Valid 5.3417
+#   - Test 5.2406
+
+python run_experiment.py \
+	--model transformer \
+	--text_encoder subword \
+	--vocab_size 8192 \
+	--epochs 38 \
+	--lr None \
+	--batch_size 64 \
+	--max_seq_length 500 \
+	--src_train data/splitted_data/train/train_token10000.en \
+	--target_train data/splitted_data/train/train_token10000.fr \
+	--src_valid data/splitted_data/valid/val_token10000.en \
+	--target_valid data/splitted_data/valid/val_token10000.fr \
+	--no_cache
