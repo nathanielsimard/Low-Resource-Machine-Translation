f443b3129b0f7343adcb583a90c990951095200c
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-16 14:57:59 +0000

    Update numbers

--- experiments/transformer/medium-subword.sh
+++ experiments/transformer/medium-subword.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Bleu Score after -- epochs:
-#   - Train --
-#   - Valid --
-#   - Test --
+# Bleu Score after 29 epochs with 15 000 vocab size:
+#   - Train 13.94
+#   - Valid 3.53
+#   - Test 3.454
+
+# Bleu Score after 13 epochs with 8192 vocab size:
+#   - Train 4.926
+#   - Valid 3.694
+#   - Test 3.28
 
 python run_experiment.py \
     --model transformer \
@@ -18,4 +23,6 @@ python run_experiment.py \
     --src_valid data/splitted_data/valid/val_token10000.en \
     --target_valid data/splitted_data/valid/val_token10000.fr \
     --hyperparameters experiments/transformer/medium-hyperparameters.json \
+    --task test \
+    --checkpoint 29 \
     --no_cache
