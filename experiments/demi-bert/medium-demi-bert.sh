4159db38551316f211a7b719708c8e48a92950aa
Tryong <olivier10_10@hotmail.com>
2020-04-16 14:09:12 -0400

    swtich for french data

--- experiments/demi-bert/medium-demi-bert.sh
+++ experiments/demi-bert/medium-demi-bert.sh
@@ -10,8 +10,8 @@ python run_experiment.py \
 	--lr None \
 	--batch_size 32 \
 	--max_seq_length 750 \
-	--src_train data/splitted_english_data/sorted_clean_train.en \
-	--src_valid data/splitted_english_data/sorted_clean_valid.en \
+	--src_train data/splitted_french_data/target_train.fr \
+	--src_valid data/splitted_french_data/target_valid.fr \
 	--hyperparameters experiments/demi-bert/medium-hyperparameters.json \
 	--std \
     --task pretraining 
