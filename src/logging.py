d2bc246f72f9f85509e3f0638c11b06f03a16e50
nathaniel <nathaniel.simard.42@gmail.com>
2020-04-18 15:15:44 -0400

    Don't write log into file when not running an experiment

--- src/logging.py
+++ src/logging.py
@@ -3,43 +3,45 @@ import os
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 
+FORMATTER = logging.Formatter("[%(asctime)s - %(levelname)s - %(name)s] %(message)s")
+
 handler = None
 level = None
 handler_std = None
 
 
-def initialize(experiment_name="experiment", debug=False, std=False):
+def initialize(experiment=None, debug=False, std=False):
     """Initialize the logging module.
 
     It can be called before any logger is created to change the default arguments.
     """
-    directory = os.path.join(
-        "logging/" + experiment_name, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    )
-    os.makedirs(directory, exist_ok=True)
-
-    file_name = os.path.join(directory, "experiment.log")
+    if experiment is not None:
+        directory = os.path.join(
+            "logging/" + experiment, datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        )
+        os.makedirs(directory, exist_ok=True)
+        file_name = os.path.join(directory, "experiment.log")
+        _initialize_handler(file_name, std)
+    else:
+        handler = logging.StreamHandler()
+        handler.setFormatter(FORMATTER)
 
     _initialize_level(debug)
-    _initialize_handler(file_name, std)
 
 
 def _initialize_handler(file_name, std):
     global handler
     global handler_std
 
-    formatter = logging.Formatter(
-        "[%(asctime)s - %(levelname)s - %(name)s] %(message)s"
-    )
     # Create a handler that rotate files each 512mb
     handler = RotatingFileHandler(
         file_name, mode="a", maxBytes=536_870_912, backupCount=4, encoding=None
     )
-    handler.setFormatter(formatter)
+    handler.setFormatter(FORMATTER)
 
     if std:
         handler_std = logging.StreamHandler()
-        handler_std.setFormatter(formatter)
+        handler_std.setFormatter(FORMATTER)
 
 
 def _initialize_level(debug):
@@ -51,7 +53,7 @@ def _initialize_level(debug):
 
 
 def create_logger(name: str) -> logging.Logger:
-    """Create a logger with default configuration and formatter."""
+    """Create a logger with default configuration and FORMATTER."""
     initialized = level is not None and handler is not None
     if not initialized:
         initialize()
