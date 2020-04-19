3f60b6fbae639d60586d3ca4a1e4005cf7919968
Jessica <jessgauvin@hotmail.com>
2020-04-19 15:50:28 -0400

    remove lstm (#88)

--- tests/model/lstm_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from src.model import lstm
-from tests.model.base import VOCAB_SIZE, MachineTranslationModelTest
-
-
-class LstmTest(MachineTranslationModelTest, unittest.TestCase):
-    def create_model(self):
-        return lstm.Lstm(VOCAB_SIZE + 1, VOCAB_SIZE + 1)
